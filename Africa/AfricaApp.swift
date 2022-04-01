2b5a690f484d00894d1fb5a72e48c4bf2dd7c040
LiemNH <liem.nguyehoang.127@gmail.com>
2022-04-01 14:44:36 +0700

    Initial Commit

---
+++ Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by LiemNH2 on 01/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
