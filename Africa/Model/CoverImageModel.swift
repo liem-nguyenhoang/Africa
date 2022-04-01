fe7b019fdb2956bca7c7e7d3433ed88e3a8bee6e
LiemNH <liem.nguyehoang.127@gmail.com>
2022-04-01 15:58:58 +0700

    [LiemNH] Decode a JSON file and fetch data

---
+++ Africa/Model/CoverImageModel.swift
@@ -0,0 +1,13 @@
+//
+//  CoverImageModel.swift
+//  Africa
+//
+//  Created by LiemNH2 on 01/04/2022.
+//
+
+import SwiftUI
+
+struct CoverImage: Codable, Identifiable {
+    let id: Int
+    let name: String
+}
