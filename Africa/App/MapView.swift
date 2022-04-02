87b518e9607ac9b8f33e7d10528acf8093d16c6c
LiemNH <liem.nguyehoang.127@gmail.com>
2022-04-02 19:02:50 +0700

    [LiemNH] Animation for MapPinker

--- Africa/App/MapView.swift
+++ Africa/App/MapView.swift
@@ -21,12 +21,12 @@ struct MapView: View {
     }()
     
     let locations: [NationalParkLocation] = Bundle.main.decode("locations.json")
-  
+    
     // MARK: - BODY
     
     var body: some View {
         // MARK: - No1 BASIC MAP
-         // Map(coordinateRegion: $region)
+        // Map(coordinateRegion: $region)
         
         // MARK: - No2 ADVANCED MAP
         Map(coordinateRegion: $region, annotationItems: locations) { item in
@@ -37,13 +37,62 @@ struct MapView: View {
             // MapMarker(coordinate: item.location, tint: .accentColor)
             
             // (C) CUSTOM BASIC ANNOTATION (it could be interactive)
+            // MapAnnotation(coordinate: item.location) {
+            //     Image("logo")
+            //         .resizable()
+            //         .scaledToFit()
+            //         .frame(width: 32, height: 32, alignment: .center)
+            // } //: ANNOTATION
+            
+            // (D) CUSTOM ADVANCED ANNOTATION (it could be interactive)
             MapAnnotation(coordinate: item.location) {
-                Image("logo")
+                MapAnnotationView(location: item)
+            }
+        } //: MAP
+        .overlay(
+            HStack {
+                Image("compass")
                     .resizable()
                     .scaledToFit()
-                    .frame(width: 32, height: 32, alignment: .center)
-            } //: ANNOTATION
-        }
+                    .frame(width: 48, height: 48, alignment: .center)
+                
+                VStack(alignment: .leading, spacing: 3) {
+                    HStack {
+                        Text("Latitude:")
+                            .font(.footnote)
+                            .fontWeight(.bold)
+                            .foregroundColor(.accentColor)
+                        Spacer()
+                        Text("\(region.center.latitude)")
+                            .font(.footnote)
+                            .foregroundColor(.white)
+                    } //: HSTACK
+                    
+                    Divider()
+                    
+                    HStack {
+                        Text("Longitude:")
+                            .font(.footnote)
+                            .fontWeight(.bold)
+                            .foregroundColor(.accentColor)
+                        Spacer()
+                        Text("\(region.center.longitude)")
+                            .font(.footnote)
+                            .foregroundColor(.white)
+                    } //: HSTACK
+                    
+                }
+            } //: HSTACK
+                .padding(.vertical, 12)
+                .padding(.horizontal, 16)
+                .background(
+                    Color.black
+                        .cornerRadius(8)
+                        .opacity(0.6)
+                )
+                .padding()
+            ,alignment: .top
+        )
     }
 }
 
