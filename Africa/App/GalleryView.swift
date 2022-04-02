6ebd931d86c0fd31f7970ff737deb9d703e39d08
LiemNH <liem.nguyehoang.127@gmail.com>
2022-04-02 22:20:27 +0700

    [LiemNH] Create a motion animation programmatically

--- Africa/App/GalleryView.swift
+++ Africa/App/GalleryView.swift
@@ -8,10 +8,22 @@
 import SwiftUI
 
 struct GalleryView: View {
+    // MARK: - PROPERTY
+    
+    
+    
+    // MARK: - BODY
+    
     var body: some View {
-        Text("Gallery")
+        ScrollView(.vertical, showsIndicators: false) {
+            Text("Gallery")
+        } //: SCROLL
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(MotionAnimationView())
     }
 }
+
+// MARK: - PREVIEW
 
 struct GalleryView_Previews: PreviewProvider {
     static var previews: some View {
